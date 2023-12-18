3e1e222284ef903f706f71ff1c0541cf767f6ea2
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-17 22:17:30 -0500

    Add function to get player choice

--- functions.js
+++ functions.js
@@ -8,15 +8,36 @@ function getComputerChoice() {
   let num = Math.floor(Math.random() * 3 + 1);
 
   if (num === 1) {
-    let choice = 'Rock'
-    console.log("Computer Choice is Rock")
+    let choice = 'Rock';
+    console.log("Computer Choice is Rock.");
   }
   else if (num === 2) {
-    let choice = 'Paper'
-    console.log("Computer Choice is Paper")
+    let choice = 'Paper';
+    console.log("Computer Choice is Paper.");
   }
   else {
-    let choice = 'Scissors'
-    console.log("Computer Choice is Scissors")
+    let choice = 'Scissors';
+    console.log("Computer Choice is Scissors.");
   }
 }
+
+function getPlayerChoice() {
+  let choice = prompt('Rock, Paper or Scissors? ');
+  choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+  
+  if (choice === 'Rock') {
+    console.log("Player Choice is Rock.");
+  }
+  else if (choice === 'Paper') {
+    console.log("Player Choice is Paper.");
+  }
+  else if (choice === 'Scissors') {
+    console.log("Player Choice is Scissors.");
+  }
+  else {
+    alert("Please enter a valid choice.");
+    getPlayerChoice();
+  }
+}
+
+getPlayerChoice();
